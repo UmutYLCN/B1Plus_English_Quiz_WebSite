3fd036581f41bc832ed5abc03cffb3cb28e46589
UmutYLCN <your-email@example.com>
2025-04-24 21:12:32 +0300

    Update Vite configuration: change base URL to '/english-quiz-app/' and set output directory to 'dist'

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/B1Plus_English_Quiz_WebSite/',
+  base: '/english-quiz-app/',
+  build: {
+    outDir: 'dist',
+  }
 })
