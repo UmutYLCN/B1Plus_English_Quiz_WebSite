33c741a4c9fa8eeeab20c9a83a56626dab4db64d
Umut <u.ylcn57@gmail.com>
2025-04-25 09:50:01 +0300

    Fix project configuration for GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
-    <App />
+    <ChakraProvider>
+      <App />
     </ChakraProvider>
   </React.StrictMode>,
 )
